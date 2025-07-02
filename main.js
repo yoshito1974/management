69658aa5f1dac5f3902d75306632e5c5f1ffe644
yoshito1974 <bistro.cavacava@gmail.com>
2025-07-03 03:34:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 (async () => {
-  
-  await liff.init({ liffId: "2007681083-EwJbXNRI" });
+  await liff.init({ liffId: "2007681083-EwJbXNRI" }); // あなたのLIFF IDに書き換えてください
 
   const form = document.getElementById("form");
 
   form.addEventListener("submit", async e => {
     e.preventDefault();
 
-    // 入力値を取得
     const data = {
       date: document.getElementById("date").value,
       store: document.getElementById("store").value,
-      person: document.getElementById("person").value,
+      lender: document.getElementById("lender").value,
+      borrower: document.getElementById("borrower").value,
       item: document.getElementById("item").value,
       price: document.getElementById("price").value
     };
 
     try {
-     
       await fetch("https://script.google.com/macros/s/AKfycbw9ScrkV7xuZGWdxL9UF1Ylioomzu6vG1WZw6poe1_TWE_31_YZJUCrizscOT4AOt4w/exec", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
       });
 
       alert("送信が完了しました！");
-      liff.closeWindow(); 
+      liff.closeWindow();
     } catch (error) {
       console.error("送信エラー:", error);
       alert("送信に失敗しました。もう一度お試しください。");
